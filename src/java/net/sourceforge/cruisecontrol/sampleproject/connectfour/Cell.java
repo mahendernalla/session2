832859192252f7c04df197e70273d03c1397de94
mahendernalla <mahenderreddynalla423@gmail.com>
2017-05-12 07:45:51 +0530

    this is my new file

--- src/java/net/sourceforge/cruisecontrol/sampleproject/connectfour/Cell.java
+++ src/java/net/sourceforge/cruisecontrol/sampleproject/connectfour/Cell.java
@@ -1,4 +1,4 @@
-/********************************************************************************
+hai this is new file/********************************************************************************
  * CruiseControl, a Continuous Integration Toolkit
  * Copyright (c) 2005, ThoughtWorks, Inc.
  * 200 E. Randolph, 25th Floor
